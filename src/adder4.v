6585153da3a33ce2faf1d40fbce02eef0ffbd859
drma01 <150511722+drma01@users.noreply.github.com>
2024-05-25 13:07:07 +0200

    Update adder4.v

--- src/adder4.v
+++ src/adder4.v
@@ -4,11 +4,12 @@ module tt_um_adder4(
     input clk,
     output [3:0] S,
     output C4
-    );
-    wire C1,C2,C3;
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
+    );
+    wire C1,C2,C3;
+   
   my_full_adder fa0 (A[0],B[0],0,  S[0],C1);
     my_full_adder fa1 (A[1],B[1],C1,S[1],C2);
     my_full_adder fa2 (A[2],B[2],C2,S[2],C3);
