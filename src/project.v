11be9be6a1b49d82b9059fd56da124487c51eeff
drma01 <150511722+drma01@users.noreply.github.com>
2024-05-27 11:19:51 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,8 +17,13 @@ module tt_um_adder4 (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    assign uo_out[7:2]  = 0;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
-
+    adder4 myadder(
+        .A (ui_in[0])
+        .B (ui_in[1])
+        .S (ui_out[0])
+        .C4 (ui_out[1])
+    );
 endmodule
